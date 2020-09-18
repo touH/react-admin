7799fa9922db0b50498437af4cbfae5d6abeee42
hhq <7900124+earthly_guest@user.noreply.gitee.com>
2020-09-18 15:09:27 +0800

    mod：utils 时间公共方法增加

--- src/utils/common/date.js
+++ src/utils/common/date.js
@@ -1,4 +1,8 @@
-// 年-月-日
+/**
+ * @description 年-月-日
+ * @param time   时间戳
+ * @return {string}
+ */
 export const getDate = time => {
   var result;
   var d = new Date(time);
@@ -19,7 +23,11 @@ export const getDate = time => {
   return result;
 };
 
-//年-月-日-时-分-秒
+/**
+ * @description 年-月-日-时-分-秒
+ * @param now    时间戳
+ * @return {string}
+ */
 export const getDateTime = function(now) {
   let year = now.getFullYear();
   let month = now.getMonth() + 1;
@@ -48,7 +56,11 @@ export const getDateTime = function(now) {
   );
 };
 
-// 时间格式化,获取时分秒
+/**
+ * @description 时间格式化,获取时分秒  这种写法是根据传入的时间戳 转为 年月日时分秒，然后获取它的是时分秒
+ * @param time
+ * @return {string|*}
+ */
 export const getBranch = time => {
   if (time == "") {
     return time;
@@ -63,7 +75,43 @@ export const getBranch = time => {
 };
 
 
-// 时间戳 转为 标准格式时间
+/**
+ * @description 传入一个毫秒数，不是时间戳。 就仅仅是毫秒转时分秒有多少，xx小时xx分xx秒   这种写法是传入毫秒 转为 时分秒
+ * @param 单纯的一个毫秒
+ * @return {string}
+ */
+function getTime(ms) {    // 后端可能会返回负数 -_-|||
+  if(ms>0) {
+    let hour, minute, second
+    let d = ms / 1000
+    second = Math.floor(d % 60)
+    minute = Math.floor((d / 60) % 60)
+    hour = Math.floor(d / 60 / 60)
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+    if (minute < 10) {
+      minute = "0" + minute;
+    }
+    if (second < 10) {
+      second = "0" + second;
+    }
+    const time = {
+      hour,
+      minute,
+      second
+    }
+    return (time.hour ? `${time.hour}小时` : '') + (time.minute ? `${time.minute}分钟` : '') + `${time.second}秒`;
+  } else {
+    return ''
+  }
+}
+
+/**
+ * @description 时间戳 转为 标准格式时间
+ * @param date
+ * @return {Date}
+ */
 export const parserDate = date => {
   var t = Date.parse(date);
   if (!isNaN(t)) {
